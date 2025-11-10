630647f297cdaf5b5cf759ba26efc9d113e96d5d
Mohammad Robiul Hossen <robim6225@gmail.com>
2025-11-10 17:18:00 +0600

    added axios custom hook

--- src/pages/AllReviews.jsx
+++ src/pages/AllReviews.jsx
@@ -1,21 +1,23 @@
-import axios from "axios";
+// import axios from "axios";
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import ReviewCard from "../components/ReviewCard";
+import useAxios from "../hooks/useAxios";
 
 const AllReviews = () => {
   const [topReview, setTopReview] = useState([]);
+  const axiosInstance = useAxios();
   useEffect(() => {
-    axios
-      .get(`http://localhost:3000/all-reviews`)
+    axiosInstance
+      .get(`/all-reviews`)
       .then((res) => {
         setTopReview(res.data);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [axiosInstance]);
   return (
     <div className="max-w-6xl mx-auto">
       <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
