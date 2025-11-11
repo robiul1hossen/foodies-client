7ec3be31fbec4adab9e05b2ce33c718309c1803a
Mohammad Robiul Hossen <robim6225@gmail.com>
2025-11-11 22:37:23 +0600

    added latest reviews section

---
+++ src/components/LatestReview.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import useAxios from "../hooks/useAxios";
+import ReviewCard from "./ReviewCard";
+
+const LatestReview = () => {
+  const axiosInstance = useAxios();
+
+  const [latest, setLatest] = useState([]);
+
+  useEffect(() => {
+    axiosInstance
+      .get(`/latest-review`)
+      .then((res) => {
+        console.log(res.data);
+        setLatest(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [axiosInstance]);
+
+  return (
+    <div>
+      <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+        {latest.map((review) => (
+          <ReviewCard key={review._id} review={review} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LatestReview;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import BannerSlider from "../components/BannerSlider";
 import TopRatedReview from "../components/TopRatedReview";
+import LatestReview from "../components/LatestReview";
 
 const Home = () => {
   return (
     <div className="max-w-6xl mx-auto">
       <BannerSlider />
       <TopRatedReview />
+      <LatestReview />
     </div>
   );
 };
